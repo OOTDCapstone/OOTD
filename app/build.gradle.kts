fe8b280f3f9cfa98c203ea3889e1ce3a0518ca30
EkaBayu <suputrabayu24@gmail.com>
2023-12-17 13:36:12 +0800

    new

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,6 +35,7 @@ android {
     }
     buildFeatures {
         viewBinding = true
+
     }
 }
 
@@ -51,4 +52,11 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    val cameraxVersion = "1.2.3"
+    implementation("androidx.camera:camera-camera2:$cameraxVersion")
+    implementation("androidx.camera:camera-lifecycle:$cameraxVersion")
+    implementation("androidx.camera:camera-view:$cameraxVersion")
+
+
+
 }
---
+++ app/src/main/java/com/example/capstoneootd/ui/camera/CameraActivity.kt
@@ -0,0 +1,12 @@
+package com.example.capstoneootd.ui.camera
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.capstoneootd.R
+
+class CameraActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_camera)
+    }
+}
