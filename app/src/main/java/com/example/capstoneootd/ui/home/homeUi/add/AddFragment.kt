5f10a68427c9dda835503f262a7300b6a5a38357
EkaBayu <suputrabayu24@gmail.com>
2023-12-19 00:22:23 +0800

    fifth commit

--- app/src/main/java/com/example/capstoneootd/ui/home/homeUi/add/AddFragment.kt
+++ app/src/main/java/com/example/capstoneootd/ui/home/homeUi/add/AddFragment.kt
@@ -1,43 +1,132 @@
 package com.example.capstoneootd.ui.home.homeUi.add
 
+import android.Manifest
+import android.app.Dialog
+import android.content.pm.PackageManager
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.Window
+import android.widget.Button
 import android.widget.TextView
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.example.bayustoryapp.data.Utils.Utils.getImageUri
+import com.example.capstoneootd.R
 import com.example.capstoneootd.databinding.FragmentAddBinding
 
 
 class AddFragment : Fragment() {
 
     private var _binding: FragmentAddBinding? = null
+    private var currentImageUri: Uri? = null
+    private val requestPermissionLauncher =
+        registerForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) { isGranted: Boolean ->
+            if (isGranted) {
+                startGallery()
+            } else {
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
+            }
+        }
+
     private val binding get() = _binding!!
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         val addViewModel =
             ViewModelProvider(this).get(AddViewModel::class.java)
-
         _binding = FragmentAddBinding.inflate(inflater, container, false)
         val root: View = binding.root
-
-
-        addViewModel.text.observe(viewLifecycleOwner) {
-
-        }
         return root
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.cameraButton.setOnClickListener {
+            showPopup()
+        }
+    }
+
+    private fun showPopup() {
+        val dialog = Dialog(requireContext())
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        dialog.setCancelable(false)
+        dialog.setContentView(R.layout.layout_popup_add)
+        dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+
+        val btnGallery: Button = dialog.findViewById(R.id.btnGallery)
+        val btnCamera: Button = dialog.findViewById(R.id.btnCamera)
+
+        btnGallery.setOnClickListener {
+            startGallery()
+            dialog.dismiss()
+        }
+        btnCamera.setOnClickListener {
+            startCamera()
+            dialog.dismiss()
+        }
+        dialog.show()
+    }
+    private fun startGallery() {
+        if (allPermissionGranted()) {
+            launcherGallery.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
+        } else {
+            requestPermissionLauncher.launch(REQUIRED_PERMISSION)
+        }
+    }
+
+    private fun startCamera() {
+        if (allPermissionGranted()) {
+            currentImageUri = getImageUri(requireContext())
+            launcherIntentCamera.launch(currentImageUri)
+        } else {
+            requestPermissionLauncher.launch(REQUIRED_PERMISSION)
+        }
+    }
+
+    private val launcherGallery = registerForActivityResult(
+        ActivityResultContracts.PickVisualMedia()
+    ) { uri: Uri? ->
+        if (uri != null) {
+            currentImageUri = uri
+            showImage()
+        } else {
+
+        }
+    }
+
+    private val launcherIntentCamera = registerForActivityResult(
+        ActivityResultContracts.TakePicture()
+    ) { isSuccess ->
+        if (isSuccess) {
+            showImage()
+        }
+    }
+
+    private fun showImage() {
+        currentImageUri?.let {uri ->
+           binding.previewImageView.setImageURI(uri)
+        }
+    }
+
+    private fun allPermissionGranted() =
+        ContextCompat.checkSelfPermission(
+            requireContext(),
+            REQUIRED_PERMISSION
+        ) == PackageManager.PERMISSION_GRANTED
+
+    companion object {
+        private const val REQUIRED_PERMISSION = Manifest.permission.CAMERA
     }
 }
+
+
